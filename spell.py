ad668279c8f621285724f6304430ffdeddc764f9
Christian Buck <cbuck@lantis.de>
2013-09-10 19:52:02 +0200

    extracted feature functions into objects.
    untested

--- spell.py
+++ spell.py
@@ -11,12 +11,16 @@ def tokens(text):
     Normalize to lowercase."""
     return re.findall('[a-z]+', text.lower())
 
-class SpellChecker(object):
-    def __init__(self, vocab_filename):
-        self.words = {}
-        for line in open(vocab_filename):
+class Vocabulary(dict):
+    def __init__(self, filename):
+        for line in open(filename):
             word, count = line.split()
-            self.words[word] = int(count)
+            self[word] = int(count)
+
+class SpellChecker(object):
+    def __init__(self, vocabulary):
+        self.words = vocabulary
+        self._word_features = []
 
     def tokens(self, line):
         return line.strip().split()
@@ -26,9 +30,53 @@ class SpellChecker(object):
         for w in self.words:
             distance = Levenshtein.distance(w, word)
             if distance <= max_distance:
-                candidates.append((w,distance, self.words[w]))
-        candidates.sort(key=itemgetter(1))
+                candidate = {'original':word, 'correction':w, 'features': []}
+                for feature in self._word_features:
+                    value = feature.value(word, w)
+                    candidate['features'].append( (feature.name(), value))
+                candidates.append(candidate)
+
+        #candidates.sort(key=itemgetter(1))
         return candidates
+
+    def register_feature(self, feature):
+        self._word_features.append(feature)
+
+    def process(self, sentence):
+        "sentence is a string"
+        sentence = tokens(line)
+        sentence = map(spell_checker.close_words, sentence)
+        return sentence
+
+class WordFeature(object):
+    """ Abstract base class of all word-level features """
+    _name = "WordFeature"
+
+    def name(self):
+        """ Name of the feature. Should not contain spaces """
+        return self._name
+
+    def value(self, word, correction):
+        """ Feature value, for example a distance """
+        return 0
+
+class EditDistanceFeature(WordFeature):
+    _name = "EditDistance"
+
+    def value(self, word, correction):
+        return Levenshtein.distance(word, correction)
+
+class CountFeature(WordFeature):
+    _name = "Count"
+
+    def __init__(self, vocabulary):
+        self.vocabulary = vocabulary
+
+    def value(self, word, correction):
+        if not correction in self.vocabulary:
+            return 0
+        else:
+            return self.vocabulary[correction]
 
 def write_hypergraph(sentence, filehandle):
     """ sentence is supposed to be a list of lists, where each position holds
@@ -41,18 +89,25 @@ def write_hypergraph(sentence, filehandle):
 
     for word_idx, possibilities in enumerate(sentence):
         filehandle.write("%d\n" %(len(possibilities)))
-        for word, distance, count in possibilities:
-            filehandle.write("[%d] %s ||| Distance=%d LogCount=%f\n"
-                             %(word_idx, word, distance, math.log(count)))
+        for candidate in possibilities:
+            features = ["=".join((f, str(val))) for f,val in candidate['features']]
+            word = candidate['correction']
+            filehandle.write("[%d] %s ||| %s\n"
+                             %(word_idx, word, " ".join(features)))
     filehandle.write("1\n")
     filehandle.write("[%d] </s> ||| \n" %(n_vertices-2))
 
 
 if __name__ == '__main__':
-    spell_checker = SpellChecker('english.vocab')
+    vocabulary = Vocabulary('english.vocab')
+    spell_checker = SpellChecker(vocabulary)
+    spell_checker.register_feature(EditDistanceFeature())
+    spell_checker.register_feature(CountFeature(vocabulary))
+
     for line in sys.stdin:
         line = line.strip()
-        sentence = tokens(line)
-        sentence = map(spell_checker.close_words, sentence)
         #print sentence
-        write_hypergraph(sentence, sys.stdout)
+        corrections = spell_checker.process(line)
+        #print corrections
+        print write_hypergraph(corrections, sys.stdout)
+        #write_hypergraph(sentence, sys.stdout)
